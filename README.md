d3ca19dd8d22b372fd43777db53f2472fed2f3be
Tamas Michelberger <tomi.michel@gmail.com>
2014-06-03 15:29:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# [swiftlytothefuture.com](http://swiftlytothefuture.com)
+
+Bullshit generator based a tweet for the new [#swiftlang](https://twitter.com/search?q=%23swiftlang) hype.
+
+## Add 'innovation' or bullshit to the list
+
+1. fork the repo
+2. add new elements to the lists in `swift.js`
+3. create a pull request
+
+Made with <3 by @tmichel and @vbalazs.
---
+++ swift.js
@@ -0,0 +1,59 @@
+(function () {
+    var innovationList = [
+            'dynamic lib loading',
+            'functional programming',
+            'no semicolons',
+            'an interactive playground',
+            'functions',
+            'variables',
+            'functions as return values',
+            'types',
+            'overflow operators',
+            'touples',
+            'labeled statements',
+            'postfix expressions',
+            'numeric type conversion',
+            'booleans',
+            'local type inference',
+            'type aliases',
+        ],
+        bullshitList = [
+            'building an alternative to the web. Native apps without installs!',
+            'curing cancer.',
+            'going to Mars.',
+            'meeting the first alien.',
+            'discovering the 8th continent.',
+            'cloudifying every device.',
+            'gamifying your scooter driving experience.',
+            'integrating user-centred networking.',
+            'engaging user-contributed network effects.',
+        ],
+        innSpan = document.getElementById('innovation'),
+        bsSpan = document.getElementById('bs'),
+        lastIn = 0,
+        lastBs = 0;
+
+        innSpan.innerText = innovationList[lastIn];
+        bsSpan.innerText = bullshitList[lastBs];
+
+        document.getElementById('btnNext').addEventListener('click', function () {
+            var newIn = rand(innovationList.length, lastIn),
+                newBs = rand(bullshitList.length, lastBs);
+
+            innSpan.innerText = innovationList[newIn];
+            bsSpan.innerText = bullshitList[newBs];
+
+            lastIn = newIn;
+            lastBs = newBs;
+        });
+
+        function rand(upper, omit) {
+            var num = Math.floor(Math.random() * upper);
+
+            while (num === omit) {
+                num = Math.floor(Math.random() * upper);
+            }
+
+            return num;
+        }
+})();
